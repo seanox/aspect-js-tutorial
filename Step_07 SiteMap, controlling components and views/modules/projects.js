144dc07096467cc7c6c6134ba262c5a1c9f025f8
seanox <service@seanox.de>
2019-12-24 21:45:48 +0100

    #0000 Tutprial: Update of content

--- Step_07 SiteMap, controlling components and views/modules/projects.js
+++
@@ -1,7 +0,0 @@
-// The click method is temporarily implemented very simply.
-// The aim of the example is the function of navigation of the SiteMap.
-window["projects"] = {
-    onClick() {
-        SiteMap.navigate("#project");
-    }     
-}
