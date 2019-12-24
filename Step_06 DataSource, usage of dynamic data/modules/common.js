48903f05c25a5e3ca37dc082b2de3ddbacad1a2a
seanox <service@seanox.de>
2019-12-24 11:24:37 +0100

    #0000 Tutprial: Update of content

---
+++ Step_06 DataSource, usage of dynamic data/modules/common.js
@@ -0,0 +1,55 @@
+/**
+ * Creates a object based on data-structures.
+ * The method is intended for deserialization.
+ * Supported data types: XMLDocument / Node
+ * @param  data
+ * @return the created object
+ */
+if (Object.parse === undefined) {
+    Object.parse = function(data) {
+
+        var append = (name, value) => {
+            if (value === null)
+                return;
+            if (object[name]) {
+                if (object[name].constructor != Array)
+                    object[name] = [object[name]];
+                object[name][object[name].length] = value;
+            } else object[name] = value;
+        };
+        
+        if (data instanceof Node) {
+            
+            if (data.nodeType == 9)
+                return DataSource.convert(data.documentElement);   
+            if (data.nodeType != 1)
+                return null;
+            
+            var meta = {text:null, data:null};
+            for (let node of data.childNodes) {
+                if (node.nodeType == 1) {
+                    meta = false;
+                    break;
+                } else if (node.nodeType == 3) {
+                    meta.text = (meta.text || "") + node.nodeValue; 
+                } else if (node.nodeType == 4) {
+                    meta.data = (meta.data || "") + node.nodeValue;
+                }
+            }
+
+            if (meta && meta.data != null)
+                return meta.data;
+            if (meta && meta.text != null)
+                return meta.text;
+
+            var object = {};
+            for (let attribute of data.attributes)
+                append(attribute.name, attribute.value);
+            for (let child of data.childNodes)
+                append(child.nodeName, Object.parse(child));
+            return object;
+        }
+        
+        throw new TypeError("Unsupported data type");
+    }
+}
