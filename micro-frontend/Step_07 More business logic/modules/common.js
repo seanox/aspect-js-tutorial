fcf23eb23e474c06bb727d157d5fd60c142c1abf
seanox <service@seanox.de>
2023-03-02 20:43:25 +0100

    #0000 Tutorial: Update of content

---
+++ micro-frontend/Step_07 More business logic/modules/common.js
@@ -0,0 +1,19 @@
+// For the HTML element with the ID error, the browser automatically creates a
+// corresponding HTML element in JavaScript. However, we want our own that uses
+// Reactive so that we don't have to worry about updating the view.
+window.error = Reactive({
+    message: null,
+    exists() {
+        return !!(this.message || "").trim();
+    }
+});
+
+Composite.listen(Composite.EVENT_ERROR, function(event, error) {
+    window.error.message = error.message;
+});
+
+Object.defineProperty(Array.prototype, "empty", {
+    get() {
+        return this.length <= 0;
+    }
+});
