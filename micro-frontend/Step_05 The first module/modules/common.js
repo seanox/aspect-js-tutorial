11dec7e6b8291192246f0ca69d3b210082ee37b8
seanox <service@seanox.de>
2023-03-18 21:01:43 +0100

    #0000 Tutorial: Update of content

--- micro-frontend/Step_05 The first module/modules/common.js
+++ micro-frontend/Step_05 The first module/modules/common.js
@@ -1,12 +1,12 @@
 // For the HTML element with the ID error, the browser automatically creates a
 // corresponding HTML element in JavaScript. However, we want our own that uses
-// the ReactProxy so that we don't have to worry about updating the view.
-window.error = ({
+// Reactive so that we don't have to worry about updating the view.
+window.error = Reactive({
     message: null,
     exists() {
         return !!(this.message || "").trim();
     }
-}).reactive();
+});
 
 Composite.listen(Composite.EVENT_ERROR, function(event, error) {
     window.error.message = error.message;
