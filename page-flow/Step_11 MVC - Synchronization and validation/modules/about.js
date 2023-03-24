d4d8ed68b78aed0021866882b3807ba69f2e00f2
seanox <service@seanox.de>
2023-03-24 07:36:52 +0100

    #0000 Tutorial: Update of content

--- page-flow/Step_11 MVC - Synchronization and validation/modules/about.js
+++ page-flow/Step_11 MVC - Synchronization and validation/modules/about.js
@@ -1,21 +1,24 @@
-window['about'] = {
+// Modules (Composite-JavaScript) use their own scope. Variables and constants
+// created and used here are not accessible outside and must be exported for use
+// in the global scope, for which the macro #export is used.
+const about = {
     contact: {
         onClick(event) {
-            var mail = event.target.getAttribute("value");
+            let mail = event.target.getAttribute("value");
             if (!mail)
                 return false;
             
-            var xml = DataSource.fetch("xml://team");
-            var xpath = "/team/person[email=\"" + mail + "\"]";
+            let xml = DataSource.fetch("xml://team");
+            let xpath = "/team/person[email=\"" + mail + "\"]";
             xml = xml.evaluate ? xml.evaluate(xpath, xml, null, XPathResult.ANY_TYPE, null) : xml.selectNodes(xpath);
-            var person = xml.iterateNext();
+            let person = xml.iterateNext();
             if (!person)
                 return;
             person = Object.parse(person);
 
-            var title = Messages["contact.mail.title"];
+            let title = Messages["contact.mail.title"];
             title = Expression.eval(title);
-            var message = Messages["contact.mail.message"];
+            let message = Messages["contact.mail.message"];
             message = message.replace(/^ +/mg, "");
             message = message.replace(/\{1\}/g, person.name.match(/^\w+/));
             
@@ -25,3 +28,5 @@ window['about'] = {
         }
     }
 }
+
+#export about;
